06bae4549f4cae39c876ad9fc75da130c165944d
alexgit2023 <alxnst07@gmail.com>
2024-06-14 20:50:06 +0900

    header in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 function App() {
   return (
     <div>
+      <header>
+        <h3>
+          Result University
+        </h3>
+        <span>Тут будет время</span>
+      </header>
       <h1>Hello React</h1>
     </div>
   )

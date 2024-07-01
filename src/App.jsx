308e997740b6369690e31021eb5ccce2f52aba0e
alexgit2023 <alxnst07@gmail.com>
2024-07-01 17:48:02 +0300

    add Differences

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 import Header from "./components/Header"
-import Button from "./components/Button/Button"
 import TeachingSection from "./components/TeachingSection"
-import { useState } from "react"
-import { differences } from './data'
+import DifferencesSection from "./components/DifferencesSection"
 
 
 export default function App() {
-  const [contentType, setContentType] = useState(null)
-
-  function handleClick(type) {
-    setContentType(type)
-  }
-
+ 
   return (
     <div>
       <Header />
       <main>
        <TeachingSection />
-        <section>
-          <h3>Чем мы отличаемся от других</h3>
-          <Button isActive={contentType === 'way'} onClick={() => handleClick('way')}>Подход</Button>
-          <Button isActive={contentType === 'easy'} onClick={() => handleClick('easy')}>Доступность</Button>
-          <Button isActive={contentType === 'program'} onClick={() => handleClick('program')}>Концентрация</Button>
-
-          {contentType ? (<p>{differences[contentType]}</p>) : (<p>Нажми на кнопку</p>)}
-          
-        </section>
+       <DifferencesSection />
       </main>
     </div>
   )

fe32f5717d08dee6d606a6f4a16748170119d280
alexgit2023 <alxnst07@gmail.com>
2024-07-01 18:09:41 +0300

    add IntroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import Header from "./components/Header"
 import TeachingSection from "./components/TeachingSection"
 import DifferencesSection from "./components/DifferencesSection"
+import IntroSection from "./components/IntroSections"
 
 
 export default function App() {
  
   return (
-    <div>
+    <>
       <Header />
       <main>
+       <IntroSection />
        <TeachingSection />
        <DifferencesSection />
       </main>
-    </div>
+    </>
   )
 }
 
---
+++ src/components/IntroSections.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+// export default function IntroSection() {
+//     return (
+//         <section>
+//             <h1 className="centered">Result University</h1>
+//             <h3 className="centered" style={{color: '#666'}}>Университет fronend-разработки, который насыщает IT-сферу квалифицированными программистами</h3>
+//         </section>
+//     )
+// }
+
+
+export default function IntroSection() {
+    return React.createElement(
+        'section', 
+        null, 
+        [React.createElement('h1', { className: 'centered' }, 'Result University'), 
+        React.createElement('h3', { className: 'centered', style: {color: '#666'} }, 'Университет fronend-разработки, который насыщает IT-сферу квалифицированными программистами')])
+}

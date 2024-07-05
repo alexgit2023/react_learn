72e2ba35dded6a43a31558cea7c76b49bcb9e410
alexgit2023 <alxnst07@gmail.com>
2024-07-05 09:46:45 +0300

    The end of cours

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
 import Button from "./Button/Button"
+import Modal from "./Modal/Modal"
 
 
 export default function EffectSection() {
+    const [modal, setModal] = useState(false)
+
+    function openModal() {
+        setModal(true)
+    }
+
+
     return (
         <section>
             <h3>Effeccts</h3>
-            
-            <Button>Открыть информацию</Button>
+
+            <Button onClick={openModal}>Открыть информацию</Button>
+
+            <Modal open={modal}>
+                <h3>Hello from modal</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, corporis hic vero cumque, iure delectus quae culpa aut temporibus quas eos numquam nesciunt, quidem eveniet cupiditate tenetur. Quos, adipisci natus.</p>
+                <Button onClick={() => setModal(false)}>Close modal</Button>
+            </Modal>
         </section>
 
     )        
